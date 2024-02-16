2a3d3b8f52b2e11bead946710646ac16f6823f5f
EkkRos <camischewa.c@yandex.ru>
2024-02-16 17:53:47 +0300

    Задача выполнена

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,17 @@
 package jm.task.core.jdbc.dao;
 
-import jm.task.core.jdbc.model.User;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory sessionFactory = Util.getSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +19,100 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        try{
+            dropUsersTable();
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            String CREATE_TABLE = "CREATE TABLE users " +
+                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                    " name VARCHAR(50), " +
+                    " last_name VARCHAR (50), " +
+                    " age INT " +
+                    ")";
+            session.createSQLQuery(CREATE_TABLE).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e){
+            System.err.println("Ошибка создания таблицы");
+        }
+
+
 
     }
 
     @Override
     public void dropUsersTable() {
+        try{
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
 
+            session.createSQLQuery("DROP TABLE IF EXISTS users;").executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (Exception e){
+            System.err.println("Ошибка удаления таблицы");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try{
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
 
+            User user = new User(name,lastName,age);
+            session.save(user);
+
+            session.getTransaction().commit();
+        } catch (Exception e){
+            System.err.println("Ошибка добавления пользователя");
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try{
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            session.delete(session.get(User.class,id));
+
+            session.getTransaction().commit();
+        } catch (Exception e){
+            System.err.println("Ошибка удаления пользователя");
+        }
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        try{
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            list = session.createQuery("from User").getResultList();
+
+            session.getTransaction().commit();
+
+        } catch (Exception e){
+            System.err.println("Ошибка удаления пользователя");
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
+        try{
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            session.createSQLQuery("DELETE FROM users").executeUpdate();
+
+            session.getTransaction().commit();
+
+        } catch (Exception e){
+            System.err.println("Ошибка удаления пользователя");
+        }
 
     }
 }
