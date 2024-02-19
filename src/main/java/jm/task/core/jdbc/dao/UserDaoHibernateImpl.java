118523f84002e97baa66d24e5e3195055fa37d3a
EkkRos <camischewa.c@yandex.ru>
2024-02-19 17:51:11 +0300

    Задача исправлена

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -19,100 +19,106 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try{
-            dropUsersTable();
-            Session session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
-            String CREATE_TABLE = "CREATE TABLE users " +
-                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
-                    " name VARCHAR(50), " +
-                    " last_name VARCHAR (50), " +
-                    " age INT " +
-                    ")";
-            session.createSQLQuery(CREATE_TABLE).executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e){
-            System.err.println("Ошибка создания таблицы");
+        dropUsersTable();
+        try(Session session = sessionFactory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
+
+                String CREATE_TABLE = "CREATE TABLE users " +
+                        "(id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                        " name VARCHAR(50), " +
+                        " last_name VARCHAR (50), " +
+                        " age INT " +
+                        ")";
+                session.createSQLQuery(CREATE_TABLE).executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                session.getTransaction().rollback();
+                System.err.println("Ошибка создания таблицы");
+            }
         }
-
-
-
     }
 
     @Override
     public void dropUsersTable() {
-        try{
-            Session session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
+        try(Session session = sessionFactory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
 
-            session.createSQLQuery("DROP TABLE IF EXISTS users;").executeUpdate();
+                session.createSQLQuery("DROP TABLE IF EXISTS users;").executeUpdate();
 
-            session.getTransaction().commit();
-        } catch (Exception e){
-            System.err.println("Ошибка удаления таблицы");
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                session.getTransaction().rollback();
+                System.err.println("Ошибка удаления таблицы");
+            }
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try{
-            Session session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
+        try(Session session = sessionFactory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
 
-            User user = new User(name,lastName,age);
-            session.save(user);
+                User user = new User(name, lastName, age);
+                session.save(user);
 
-            session.getTransaction().commit();
-        } catch (Exception e){
-            System.err.println("Ошибка добавления пользователя");
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                session.getTransaction().rollback();
+                System.err.println("Ошибка добавления пользователя");
+            }
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        try{
-            Session session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
+        try(Session session = sessionFactory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
 
-            session.delete(session.get(User.class,id));
+                session.delete(session.get(User.class,id));
 
-            session.getTransaction().commit();
-        } catch (Exception e){
-            System.err.println("Ошибка удаления пользователя");
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                session.getTransaction().rollback();
+                System.err.println("Ошибка удаления пользователя");
+            }
         }
-
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
-        try{
-            Session session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
+        try(Session session = sessionFactory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
 
-            list = session.createQuery("from User").getResultList();
+                list = session.createQuery("from User").getResultList();
 
-            session.getTransaction().commit();
-
-        } catch (Exception e){
-            System.err.println("Ошибка удаления пользователя");
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                session.getTransaction().rollback();
+                System.err.println("Ошибка получения всех пользователей");
+            }
         }
         return list;
     }
 
     @Override
     public void cleanUsersTable() {
-        try{
-            Session session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
+        try(Session session = sessionFactory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
 
-            session.createSQLQuery("DELETE FROM users").executeUpdate();
+                session.createSQLQuery("DELETE FROM users").executeUpdate();
 
-            session.getTransaction().commit();
-
-        } catch (Exception e){
-            System.err.println("Ошибка удаления пользователя");
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                session.getTransaction().rollback();
+                System.err.println("Ошибка очистки таблицы");
+            }
         }
-
     }
 }
